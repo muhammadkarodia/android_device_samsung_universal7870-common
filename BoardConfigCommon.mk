8f3dd414d7743e75b96bd5427c5a7a36764028dc
debie | a3y17ltexc <79523414+debie-A320FL@users.noreply.github.com>
2021-09-25 13:53:57 +0200

    update fingerprint

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -187,7 +187,7 @@ TARGET_LD_SHIM_LIBS += \
     /system/bin/mediaserver|/system/lib/libstagefright_shim.so
 
 # Build fingerprint
-BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210705.001/7380771:user/release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210905.001/7511028:user/release-keys"
 
 # Wifi
 BOARD_HAVE_SAMSUNG_WIFI := true
